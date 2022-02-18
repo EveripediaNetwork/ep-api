5c2819678377723b1d0ff1b4c61e2cabc706b3b3
AshakaE <ashakaegerega@gmail.com>
2022-02-18 09:32:24 +0100

    Simplify indexer

--- src/ipfs-hash/ipfs-hash.service.ts
+++ src/ipfs-hash/ipfs-hash.service.ts
@@ -13,12 +13,38 @@ interface Valid {
   }
 }
 
+interface GetHashs {
+  ipfshashs: [{ id: string }]
+}
+
 @Injectable()
 export default class IpfsHashService {
   constructor(
     @InjectModel(HashIndex)
     private hashIndexModel: typeof HashIndex,
   ) {}
+
+  public async getHashes(arg: string, url: string): Promise<GetHashs> {
+    return request(url, arg)
+  }
+
+  public async filterValidHashes(arg: any[]): Promise<any[]> {
+    const ress: [{ id: string }] = [
+      { id: 'QmUGs99gpmE2KKh454rs4DYvwrVLbq44i8DvWDqgBEy3Qs' },
+    ]
+    const result = arg.filter(
+      (el: { id: string }) => el.id.length === 46 && el.id.startsWith('Qm'),
+    )
+    return ress.concat(result)
+  }
+
+  public async queryIpfs(arg: any[], url: string): Promise<any[]> {
+    const queryPromise = []
+    for (let i = 0; i < arg.length; i += 1) {
+      queryPromise.push(axios.get(`${url}/${arg[i].id}`))
+    }
+    return Promise.all(queryPromise)
+  }
 
   async indexHash(): Promise<HashIndex[]> {
     const query = gql`
@@ -28,31 +54,22 @@ export default class IpfsHashService {
         }
       }
     `
-
-    const res = await request(
-      'https://api.thegraph.com/subgraphs/name/kesar/wiki-mumbai-v1',
+    const res = await this.getHashes(
       query,
+      'https://api.thegraph.com/subgraphs/name/kesar/wiki-mumbai-v1',
     )
 
     // Filter valid ipfshashes
     const { ipfshashs } = res
-    const ress: [{ id: string }] = [
-      { id: 'QmUGs99gpmE2KKh454rs4DYvwrVLbq44i8DvWDqgBEy3Qs' },
-    ]
-    const result = ipfshashs.filter(
-      (el: { id: string }) => el.id.length === 46 && el.id.startsWith('Qm'),
-    )
-    ress.concat(result)
+
+    const recess = await this.filterValidHashes(ipfshashs)
 
     // Query ipfs
-    const queryPromise = []
-    for (let i = 0; i < ress.length; i += 1) {
-      queryPromise.push(
-        axios.get(`https://gateway.pinata.cloud/ipfs/${ress[i].id}`),
-      )
-    }
-
-    let queryHash = await Promise.all(queryPromise)
+    let queryHash = await this.queryIpfs(
+      recess,
+      // 'https://ipfs.io/ipfs',
+      'https://gateway.pinata.cloud/ipfs',
+    )
     queryHash = queryHash.map(el => el.data)
 
     // filter hashes with users only
