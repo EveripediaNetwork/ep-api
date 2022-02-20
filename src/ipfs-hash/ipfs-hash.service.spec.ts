7a6b00c5a09d59f47f384b94dd298cd844d3be85
AshakaE <ashakaegerega@gmail.com>
2022-02-20 21:59:08 +0100

    Setup ipfs hash test suite

---
+++ src/ipfs-hash/ipfs-hash.service.spec.ts
@@ -0,0 +1,27 @@
+// import { SequelizeModule } from '@nestjs/sequelize'
+import { getModelToken } from '@nestjs/sequelize'
+import { Test, TestingModule } from '@nestjs/testing'
+import IpfsHashService from './ipfs-hash.service'
+import Hash from './models/hashIndex.model'
+
+describe('IpfsHashService', () => {
+  let service: IpfsHashService
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        IpfsHashService,
+        {
+          provide: getModelToken(Hash),
+          useValue: Hash,
+        },
+      ],
+    }).compile()
+
+    service = module.get<IpfsHashService>(IpfsHashService)
+  })
+
+  it('should be defined', () => {
+    expect(service).toBeDefined()
+  })
+})
