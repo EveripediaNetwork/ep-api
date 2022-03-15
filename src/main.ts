3d007137cc7e1c3c1d764682f6f8a3c0d5cd0f62
Cesar Rodriguez <kesarr@gmail.com>
2022-03-15 16:25:42 +0100

    fix ssl

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import { NestFactory } from '@nestjs/core'
+import fs from 'fs'
 import AppModule from './App/app.module'
 import config from './config'
-import fs from 'fs'
 
 async function bootstrap() {
-  const app = (Number(config.port) === 443) ? await NestFactory.create(AppModule, {
-    httpsOptions: {
-      cert: fs.readFileSync('../fullchain.pem'),
-      key: fs.readFileSync('../privkey.pem'),
-    },
-  }) : await NestFactory.create(AppModule)
+  const app =
+    Number(config.port) === 443
+      ? await NestFactory.create(AppModule, {
+          httpsOptions: {
+            cert: fs.readFileSync('../fullchain.pem'),
+            key: fs.readFileSync('../privkey.pem'),
+          },
+        })
+      : await NestFactory.create(AppModule)
   app.enableCors()
   await app.listen(config.port)
-
 }
 
 bootstrap()
