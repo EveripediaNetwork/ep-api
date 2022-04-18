d54a98ef579ea0538dbdcff4ea6e47a7d6eaa8b8
Philipe <philipep.galdino@gmail.com>
2022-04-18 19:46:12 -0300

    hotfix: added ignore_watch on uploads folder and files on pm2 service

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,15 +4,17 @@ module.exports = {
       name: 'ep-api',
       script: 'yarn dev',
       watch: true,
+      ignore_watch: ["[\/\\]\./", "uploads"],
     },
     {
       name: 'indexer-service',
       script: 'yarn console:dev indexer -l true',
       args: 'indexer',
       watch: true,
+      ignore_watch: ["[\/\\]\./", "uploads"],
       watch_delay: 1000,
       restart_delay: 900000,
-      max_memory_restart: '300M'
+      max_memory_restart: '400M',
     }
   ]
 }
