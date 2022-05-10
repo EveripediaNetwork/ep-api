3354ede484b9befedc24f86166a9b3e18cae829c
AshakaE <ashakaegerega@gmail.com>
2022-05-10 22:21:24 +0100

    Add volume change

--- src/App/tokenStats/CryptoStats/stats-getter.service.ts
+++ src/App/tokenStats/CryptoStats/stats-getter.service.ts
@@ -23,30 +23,44 @@ class StatsGetterService {
     return response
   }
 
-  private cgApiCall(name: string) {
-      const url = `1https://api.coingecko.com/api/v3/coins/${name}?tickers=false&community_data=false&developer_data=false&sparkline=false`
-      const response = this.httpService.get(url)
-      return response
+  private async cgApiCall(name: string) {
+    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${name}&order=market_cap_desc&per_page=50&page=1&sparkline=false&price_change_percentage=24h`
+    const urll = `https://api.coingecko.com/api/v3/coins/${name}/market_chart?vs_currency=usd&days=1&interval=daily`
+    let response: any = {}
+    const marketValueChange = this.httpService.get(url)
+    const volumeChange = this.httpService.get(urll)
+    const marketValueResult = await lastValueFrom(marketValueChange)
+    const volumeChangeResult = await lastValueFrom(volumeChange)
+    response = { ...marketValueResult.data, ...volumeChangeResult.data }
+    return response
   }
 
   async getStats(name: string): Promise<any> {
     const cmc = await lastValueFrom(this.cmcApiCall(name))
-    const cg = await lastValueFrom(this.cgApiCall(name))
-    console.log(cg)
+    const cg = await this.cgApiCall(name)
+
     const data = { ...cmc.data }
     const res: any = Object.values(data.data)
     const cmcData: any = res[0].quote.USD
+
+    const cgMarketData: any = Object.values(cg)[0]
+    const volumeChange =
+      ((cg.total_volumes[1][1] - cg.total_volumes[0][1]) /
+        cg.total_volumes[0][1]) *
+      100
 
     const tokenStats: TokenData = {
       id: res[0].slug,
       symbol: res[0].symbol,
       name: res[0].name,
       market_cap: cmcData.market_cap,
-      market_cap_percentage_change: cg.data.market_data.market_cap_change_percentage_24h,
+      market_cap_percentage_change:
+        cgMarketData.market_cap_change_percentage_24h,
       diluted_market_cap: cmcData.fully_diluted_market_cap,
-      diluted_market_cap_percentage_change: cg.data.market_data.market_cap_change_percentage_24h,
+      diluted_market_cap_percentage_change:
+        cgMarketData.market_cap_change_percentage_24h,
       volume: cmcData.volume_24h,
-      volume_percentage_change: 0,
+      volume_percentage_change: volumeChange,
     }
     return tokenStats
   }
