4847ab1045197ade44873c4e233ea5c280d4891f
Philipe <philipep.galdino@gmail.com>
2022-05-17 15:09:02 -0300

    update: added metadata types as enums in Wiki types

--- src/types/IWiki.ts
+++ src/types/IWiki.ts
@@ -1,3 +1,20 @@
+export enum CommonMetaIds {
+  PAGE_TYPE = 'page-type',
+  REFERENCES = 'references',
+  FACEBOOK_PROFILE = 'facebook_profile',
+  INSTAGRAM_PROFILE = 'instagram_profile',
+  TWITTER_PROFILE = 'twitter_profile',
+  LINKEDIN_PROFILE = 'linkedin_profile',
+  YOUTUBE_PROFILE = 'youtube_profile',
+}
+
+export enum EditSpecificMetaIds {
+  COMMIT_MESSAGE = 'commit-message',
+  WORDS_CHANGED = 'words-changed',
+  PERCENT_CHANGED = 'percent-changed',
+  BLOCKS_CHANGED = 'blocks-changed',
+}
+
 export interface IWiki {
   id: string
   version: number
