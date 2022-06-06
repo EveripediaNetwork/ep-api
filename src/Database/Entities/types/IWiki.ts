8817398cd258a0487bcd25fb18714ea4a3cdf8c0
AshakaE <ashakaegerega@gmail.com>
2022-06-06 12:19:27 +0100

    Add metadata ids

--- src/Database/Entities/types/IWiki.ts
+++ src/Database/Entities/types/IWiki.ts
@@ -16,3 +16,29 @@ export enum ValidatorCodes {
   TAG = 'TAG ERROR',
   URL = 'EXTERNAL URL ERROR',
 }
+
+export enum CommonMetaIds {
+  PAGE_TYPE = 'page-type',
+  REFERENCES = 'references',
+  FACEBOOK_PROFILE = 'facebook_profile',
+  INSTAGRAM_PROFILE = 'instagram_profile',
+  TWITTER_PROFILE = 'twitter_profile',
+  LINKEDIN_PROFILE = 'linkedin_profile',
+  YOUTUBE_PROFILE = 'youtube_profile',
+}
+
+export enum EditSpecificMetaIds {
+  COMMIT_MESSAGE = 'commit-message',
+  WORDS_CHANGED = 'words-changed',
+  PERCENT_CHANGED = 'percent-changed',
+  BLOCKS_CHANGED = 'blocks-changed',
+}
+
+export enum PageTypeName {
+  GENERIC = 'generic',
+  PERSON = 'Person',
+  EVENT = 'Event',
+  DAPP = 'Dapp',
+  NFT = 'NFT',
+  TOKEN = 'Token',
+}
