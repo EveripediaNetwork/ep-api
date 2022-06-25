f44334110aea1fad9ad6d0eea4614806f572faef
AshakaE <ashakaegerega@gmail.com>
2022-06-25 12:01:39 +0100

    Add other options

--- src/Database/Entities/userprofile.entity.ts
+++ src/Database/Entities/userprofile.entity.ts
@@ -2,11 +2,21 @@ import { Column, Entity, PrimaryColumn } from 'typeorm'
 import { Field, ID, ObjectType } from '@nestjs/graphql'
 
 interface Links {
-    instagram?: string
-    twitter?: string
-    website?: string
+  instagram?: string
+  twitter?: string
+  website?: string
 }
 
+interface Notifications {
+    EVERIPEDIA_NOTIFICATIONS: boolean
+    WIKI_OF_THE_DAY: boolean
+    WIKI_OF_THE_MONTH: boolean
+    EDIT_NOTIFICATIONS: boolean
+}
+
+interface AdvancedSettings {
+    SIGN_EDITS_WITH_RELAYER: boolean
+}
 
 @ObjectType()
 @Entity()
@@ -28,7 +38,7 @@ class Userprofile {
 
   @Field()
   @Column('varchar', {
-    length: 100
+    length: 100,
   })
   email!: string
 
@@ -47,6 +57,14 @@ class Userprofile {
     length: 46,
   })
   banner!: string
+
+  @Field()
+  @Column('jsonb')
+  nottifications!: Notifications
+
+  @Field()
+  @Column('jsonb')
+  advancedSettings!: AdvancedSettings
 }
 
 export default Userprofile
