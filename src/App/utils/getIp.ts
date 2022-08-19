88a0ca1b4c978de88921c5d37f4f9a477c24b739
AshakaE <ashakaegerega@gmail.com>
2022-08-19 15:17:53 +0100

    Resolve undefined method

--- src/App/utils/getIp.ts
+++ src/App/utils/getIp.ts
@@ -1,9 +1,9 @@
 import { createParamDecorator } from '@nestjs/common'
 import * as requestIp from 'request-ip'
 
-const IpAddress = createParamDecorator((req) => {
+const IpAddress = createParamDecorator((data, req) => {
   if (req.clientIp) return req.clientIp
- return  requestIp.getClientIp(req)
+  return requestIp.getClientIp(req)
 })
 
 export default IpAddress
