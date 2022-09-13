bb7057aa9b83f44dbfb285106eefe36e219257b8
Ashaka Egerega <ashakaegerega@gmail.com>
2022-09-13 18:41:19 +0100

    Revert "Fastify (#196)" (#197)
    This reverts commit 3264c73be48171467d9fe290f5a503a77721ee85.

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,26 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify'
 import fs from 'fs'
 import { ValidationPipe } from '@nestjs/common'
 import * as Sentry from '@sentry/node'
 import AppModule from './App/app.module'
 
 async function bootstrap() {
-  let app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  )
+  let app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
 
   const port = configService.get<number>('PORT')
 
-  if (Number(port) === 443) {
-    const httpsOptions = {
-      cert: fs.readFileSync('../fullchain.pem'),
-      key: fs.readFileSync('../privkey.pem'),
-    }
-    app = await NestFactory.create<NestFastifyApplication>(
-      AppModule,
-      new FastifyAdapter({ https: httpsOptions }),
-    )
-  } else {
-    app = await NestFactory.create<NestFastifyApplication>(
-      AppModule,
-      new FastifyAdapter({ logger: true }),
-    )
-  }
+  app =
+    Number(port) === 443
+      ? await NestFactory.create(AppModule, {
+          httpsOptions: {
+            cert: fs.readFileSync('../fullchain.pem'),
+            key: fs.readFileSync('../privkey.pem'),
+          },
+        })
+      : await NestFactory.create(AppModule)
+
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
   Sentry.init({
