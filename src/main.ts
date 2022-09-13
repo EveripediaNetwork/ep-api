3264c73be48171467d9fe290f5a503a77721ee85
Ashaka Egerega <ashakaegerega@gmail.com>
2022-09-13 18:30:19 +0100

    Fastify (#196)
    * fastify adapter
    
    * Update packages
    
    * Change version
    
    * Disable apollo cors

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,38 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
 import fs from 'fs'
 import { ValidationPipe } from '@nestjs/common'
 import * as Sentry from '@sentry/node'
 import AppModule from './App/app.module'
 
 async function bootstrap() {
-  let app = await NestFactory.create(AppModule)
+  let app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  )
   const configService = app.get(ConfigService)
 
   const port = configService.get<number>('PORT')
 
-  app =
-    Number(port) === 443
-      ? await NestFactory.create(AppModule, {
-          httpsOptions: {
-            cert: fs.readFileSync('../fullchain.pem'),
-            key: fs.readFileSync('../privkey.pem'),
-          },
-        })
-      : await NestFactory.create(AppModule)
-
+  if (Number(port) === 443) {
+    const httpsOptions = {
+      cert: fs.readFileSync('../fullchain.pem'),
+      key: fs.readFileSync('../privkey.pem'),
+    }
+    app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter({ https: httpsOptions }),
+    )
+  } else {
+    app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter({ logger: true }),
+    )
+  }
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
   Sentry.init({
