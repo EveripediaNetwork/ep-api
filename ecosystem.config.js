844b634ab88677f44de456642183d5fc922de640
César Rodríguez <kesarr@gmail.com>
2022-09-14 13:54:20 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
   apps:  [
     {
       name: 'ep-api',
-      script: 'yarn start:prod',
+      script: 'dist/src/main.js',
       watch: false,
       instances : "max",
       exec_mode : "cluster",
       ignore_watch: ["[\/\\]\./", "uploads"],
       restart_delay: 10000,
       watch_delay: 10000,
-      max_memory_restart: '400M',
+      max_memory_restart: '600M',
     },
     {
       name: 'indexer-service',
