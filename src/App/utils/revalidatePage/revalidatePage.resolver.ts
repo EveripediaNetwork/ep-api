89ec73d9edf15f4f2e49034a01f08a29feec91e4
AshakaE <ashakaegerega@gmail.com>
2022-09-19 09:19:18 +0100

    Protect mutation

--- src/App/utils/revalidatePage/revalidatePage.resolver.ts
+++ src/App/utils/revalidatePage/revalidatePage.resolver.ts
@@ -1,7 +1,8 @@
-import { UseInterceptors } from '@nestjs/common'
+import { UseGuards, UseInterceptors } from '@nestjs/common'
 import { Args, Mutation, Resolver, ArgsType, Field } from '@nestjs/graphql'
 
 import SentryInterceptor from '../../../sentry/security.interceptor'
+import AuthGuard from '../admin.guard'
 import { RevalidatePageService, Routes } from './revalidatePage.service'
 
 @ArgsType()
@@ -22,6 +23,7 @@ class RevalidatePageResolver {
   constructor(private revalidateService: RevalidatePageService) {}
 
   @Mutation(() => Boolean)
+  @UseGuards(AuthGuard)
   async revalidatePage(@Args() args: RouteArgs) {
     await this.revalidateService.revalidate(args.route, args.id, args.slug)
     return true
