4f8ebc266b89cf8be455ab85e6a610923e21534f
César Rodríguez <kesarr@gmail.com>
2022-09-21 19:39:53 +0200

    Revert "Feature/api performance (#206)" (#207)
    This reverts commit d122a7b54f1ec5c2953098bf929e12d11629c2a3.

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,25 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify'
 import fs from 'fs'
 import { ValidationPipe } from '@nestjs/common'
 import * as Sentry from '@sentry/node'
 import AppModule from './App/app.module'
 
 async function bootstrap() {
-  let app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  )
+  let app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
 
   const port = configService.get<number>('PORT')
 
   app =
     Number(port) === 443
-      ? await NestFactory.create<NestFastifyApplication>(
-          AppModule,
-          new FastifyAdapter(),
-          {
-            httpsOptions: {
-              cert: fs.readFileSync('../fullchain.pem'),
-              key: fs.readFileSync('../privkey.pem'),
-            },
+      ? await NestFactory.create(AppModule, {
+          httpsOptions: {
+            cert: fs.readFileSync('../fullchain.pem'),
+            key: fs.readFileSync('../privkey.pem'),
           },
-        )
-      : await NestFactory.create<NestFastifyApplication>(
-          AppModule,
-          new FastifyAdapter(),
-        )
+        })
+      : await NestFactory.create(AppModule)
 
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
