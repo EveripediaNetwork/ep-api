0ea646d08278a5ebb625f607ff2059008f6ab02d
Ashaka Egerega <ashakaegerega@gmail.com>
2022-09-23 19:43:22 +0100

    Test fastify (#216)

--- src/Database/ormconfig.ts
+++ src/Database/ormconfig.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core'
+import {
+  NestFastifyApplication,
+  FastifyAdapter,
+} from '@nestjs/platform-fastify'
 import { TYPEORM_MODULE_OPTIONS } from '@nestjs/typeorm/dist/typeorm.constants'
 import AppModule from '../App/app.module'
 
 const setup = async () => {
-  const app = await NestFactory.create(AppModule)
+   const app = await NestFactory.create<NestFastifyApplication>(
+     AppModule,
+     new FastifyAdapter(),
+   )
   const typeOrmModuleOptions = app.get(TYPEORM_MODULE_OPTIONS)
   await app.close()
 
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,46 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import fs from 'fs'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
 import { ValidationPipe } from '@nestjs/common'
 import * as Sentry from '@sentry/node'
 import AppModule from './App/app.module'
 
 async function bootstrap() {
-  let app = await NestFactory.create(AppModule)
+  let app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  )
   const configService = app.get(ConfigService)
 
   const port = configService.get<number>('PORT')
 
   app =
     Number(port) === 443
-      ? await NestFactory.create(AppModule, {
-          httpsOptions: {
-            cert: fs.readFileSync('../fullchain.pem'),
-            key: fs.readFileSync('../privkey.pem'),
+      ? await NestFactory.create<NestFastifyApplication>(
+          AppModule,
+          new FastifyAdapter(),
+          {
+            httpsOptions: {
+              cert: fs.readFileSync('../fullchain.pem'),
+              key: fs.readFileSync('../privkey.pem'),
+            },
           },
-        })
-      : await NestFactory.create(AppModule)
+        )
+      : await NestFactory.create<NestFastifyApplication>(
+          AppModule,
+          new FastifyAdapter(),
+        )
 
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
   Sentry.init({
     dsn: configService.get<string>('SENTRY_DSN'),
   })
-  await app.listen(port || 5000)
+  await app.listen(port || 5000, '0.0.0.0')
 }
 
 bootstrap()
