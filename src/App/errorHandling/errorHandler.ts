a7e1c01443bf6218e762180010f45eaa68eeee70
AshakaE <ashakaegerega@gmail.com>
2022-09-23 15:41:18 +0100

    Remove deps

--- src/App/errorHandling/errorHandler.ts
+++ src/App/errorHandling/errorHandler.ts
@@ -1,15 +1,14 @@
 import { HttpException } from '@nestjs/common'
 
 export enum ErrorTypes {
-    NOT_FOUND = 'NotFoundError'
+  NOT_FOUND = 'NotFoundError',
 }
-
 
 export class GqlError extends HttpException {
   constructor(
     statusCode: number,
-    objectOrError?: string | object | any ,
-    description?: string
+    objectOrError?: string | object | any,
+    description?: string,
   ) {
     super(
       HttpException.createBody(objectOrError, description, statusCode),
@@ -17,4 +16,3 @@ export class GqlError extends HttpException {
     )
   }
 }
-
