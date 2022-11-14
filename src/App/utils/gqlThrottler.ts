bbf1406ed5c8ed3b82abb496b2d6b66745a9d81d
César Rodríguez <kesarr@gmail.com>
2022-11-14 14:06:17 +0100

    Update gqlThrottler.ts

--- src/App/utils/gqlThrottler.ts
+++ src/App/utils/gqlThrottler.ts
@@ -4,6 +4,10 @@ import { ThrottlerGuard } from '@nestjs/throttler'
 
 @Injectable()
 export default class GqlThrottlerGuard extends ThrottlerGuard {
+  protected getTracker(req: Record<string, any>): string {
+    console.log(req.ips)
+    return req.ips.length ? req.ips[0] : req.ip // individualize IP extraction to meet your own needs
+  }
   getRequestResponse(context: ExecutionContext) {
     const gqlCtx = GqlExecutionContext.create(context)
     const ctx = gqlCtx.getContext()
