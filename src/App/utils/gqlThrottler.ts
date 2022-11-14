55d6d6c94412ca27dab98f6e8a521095eff36482
César Rodríguez <kesarr@gmail.com>
2022-11-14 14:18:18 +0100

    Update gqlThrottler.ts

--- src/App/utils/gqlThrottler.ts
+++ src/App/utils/gqlThrottler.ts
@@ -5,17 +5,11 @@ import { ThrottlerGuard } from '@nestjs/throttler'
 @Injectable()
 export default class GqlThrottlerGuard extends ThrottlerGuard {
   protected getTracker(req: Record<string, any>): string {
-    console.log(req.ips)
-    console.log(req.headers['x-forwarded-for'])
-    console.log(req.connection.remoteAddress)
     return req.headers['x-forwarded-for'] || req.ip
   }
   getRequestResponse(context: ExecutionContext) {
     const gqlCtx = GqlExecutionContext.create(context)
     const ctx = gqlCtx.getContext()
-    console.log(ctx.req.ip)
-    console.log(ctx.req.headers['x-forwarded-for'])
-    console.log(ctx.req.connection.remoteAddress)
     return { req: ctx.req, res: ctx.req.res }
   }
 }
