6de6ea5e76b6257cccae08c4eadb688ba03faa01
Drebakare <bakaredmlr@gmail.com>
2023-01-02 13:16:47 +0100

    adds last mod

--- src/Sitemap/controllers/sitemap.controller.ts
+++ src/Sitemap/controllers/sitemap.controller.ts
@@ -4,17 +4,21 @@ import staticPagesData from '../data/staticPagesData'
 
 @Controller()
 export default class SitemapController {
+  sitemapXmlCache: any;
+  sitemapTimeoutMs = 1000 * 60 * 60;
   @Get('sitemap')
   async sitemap(@Response() res: any) {
     res.set('Content-Type', 'text/xml')
     const smStream = new SitemapStream({
       hostname: 'https://iq.wiki',
+      lastmodDateOnly: true
     })
     staticPagesData.map(url =>
       smStream.write({
         url,
         changefreq: 'monthly',
         priority: 0.7,
+        lastmod: new Date()
       }),
     )
     smStream.end()
