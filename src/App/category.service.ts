26251e36e8d80b0f0f2397b484fa16dc9ae8710f
Drebakare <bakaredmlr@gmail.com>
2023-01-03 15:13:06 +0100

    refactors category service

--- src/App/category.service.ts
+++ src/App/category.service.ts
@@ -1,14 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { ConfigService } from '@nestjs/config'
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { Connection } from 'typeorm'
-import TokenValidator from './utils/validateToken'
 import Category from '../Database/Entities/category.entity'
 
 @Injectable()
 class CategoryService {
   constructor(private connection: Connection) {}
-  async wikisIds() {
+  async categoriesIds() {
     const repository = this.connection.getRepository(Category)
     return repository.find({
       select: ['id'],
