99d02ad80e036ea2be2cd469b551d583ca5d5808
Bakare Damilare E <70170061+Emmanueldmlr@users.noreply.github.com>
2023-01-04 12:32:54 +0100

    Create sitemap (#279)
    * implements sitemap for static pages
    
    * fixes lint error
    
    * adds last mod
    
    * fixes lint error
    
    * caches sitemap
    
    * fixes lint error
    
    * fixes lint error
    
    * fixes lint error
    
    * removes private pages from sitemap
    
    * implements sitemap for wikis
    
    * fixes lint error
    
    * fixes lint error
    
    * creates Category Service
    
    * refactors category service
    
    * removes unused imports
    
    * adds category service to app module
    
    * implements sitemap for categories
    
    * fixes lint error
    
    * reworks sitemap controller
    
    * fixes lint error
    
    * fixes type error
    
    * fixes lint error
    
    * reworks wiki sitemap lastmod to use updated time
    
    * reduces category sitemap priority
    
    * reworks static pages data sitemap
    
    * fixes lint error
    
    * Update src/Sitemap/data/staticPagesData.ts
    
    * shows only active categories
    
    * fixes lint error
    
    Co-authored-by: César Rodríguez <kesarr@gmail.com>

---
+++ src/App/category.service.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Injectable } from '@nestjs/common'
+import { Connection, MoreThan } from 'typeorm'
+import Category from '../Database/Entities/category.entity'
+
+@Injectable()
+class CategoryService {
+  constructor(private connection: Connection) {}
+
+  async categoriesIds() {
+    const repository = this.connection.getRepository(Category)
+    return repository.find({
+      select: ['id'],
+      where: {
+        weight: MoreThan(0),
+      },
+    })
+  }
+}
+
+export default CategoryService
---
+++ src/Sitemap/controllers/sitemap.controller.ts
@@ -0,0 +1,70 @@
+import { Controller, Get, Response } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { SitemapStream, streamToPromise } from 'sitemap'
+import CategoryService from '../../App/category.service'
+import WikiService from '../../App/wikis.service'
+import staticPagesData from '../data/staticPagesData'
+
+@Controller()
+export default class SitemapController {
+  sitemapXmlCache!: Buffer | null
+
+  sitemapTimeoutMs = 1000 * 60 * 60
+
+  lastmod = new Date()
+
+  constructor(
+    private wikiService: WikiService,
+    private configService: ConfigService,
+    private categoryService: CategoryService,
+  ) {}
+
+  @Get('sitemap')
+  async sitemap(@Response() res: any) {
+    res.set('Content-Type', 'text/xml')
+    if (this.sitemapXmlCache) {
+      res.send(this.sitemapXmlCache)
+      return
+    }
+    const smStream = new SitemapStream({
+      hostname: this.configService.get('WEBSITE_URL'),
+      lastmodDateOnly: true,
+    })
+    staticPagesData.map(data =>
+      smStream.write({
+        url: data.url,
+        changefreq: data.changeFreq,
+        priority: data.priority,
+        lastmod: this.lastmod,
+      }),
+    )
+    const [wikisIds, categoriesId] = await Promise.all([
+      this.wikiService.wikisIds(),
+      this.categoryService.categoriesIds(),
+    ])
+    wikisIds.map(wiki =>
+      smStream.write({
+        url: `/wiki/${wiki.id}`,
+        changefreq: 'daily',
+        priority: 1,
+        lastmod: wiki.updated,
+      }),
+    )
+    categoriesId.map(category =>
+      smStream.write({
+        url: `/categories/${category.id}`,
+        changefreq: 'weekly',
+        priority: 0.6,
+        lastmod: this.lastmod,
+      }),
+    )
+    smStream.end()
+    streamToPromise(smStream).then((xml: Buffer) => {
+      this.sitemapXmlCache = xml
+      setTimeout(() => {
+        this.sitemapXmlCache = null
+      }, this.sitemapTimeoutMs)
+      res.send(xml)
+    })
+  }
+}
