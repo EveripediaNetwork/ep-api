db9617294e5c88df21f973a6e67e59ccfaa08303
Ashaka Egerega <ashakaegerega@gmail.com>
2023-02-08 09:47:38 +0100

    Winston log update (#308)
    * Log requests
    
    * Log requests
    
    * Setup logs for filebeat
    
    * Setup logs for filebeat
    
    * Delete logs test controller
    
    * Update logger
    
    * EOL
    
    * Check if log file exists  before logging requests

--- src/App/utils/logger.ts
+++ src/App/utils/logger.ts
@@ -2,23 +2,31 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import ecsFormat from '@elastic/ecs-winston-format'
 import { NextFunction } from 'express'
+import fs from 'fs'
 import winston from 'winston'
 
-const winstonLog = winston.createLogger({
-  level: 'debug',
-  format: ecsFormat({ convertReqRes: true }),
-  transports: [
-    new winston.transports.File({
-      filename: 'logs/log.json',
-      level: 'debug',
-    }),
-  ],
-})
+const winstonLog = () =>
+  winston.createLogger({
+    level: 'info',
+    format: ecsFormat({ convertReqRes: true }),
+    transports: [
+      new winston.transports.File({
+        filename: 'logs/log.json',
+        level: 'info',
+      }),
+    ],
+  })
+
+winstonLog()
 
 const logger = (req: any, res: any, next: NextFunction) => {
+  const logFile = `${process.cwd()}/logs/log.json`
+
+  if (!fs.existsSync(logFile)) winstonLog()
+
   if (req?.body?.operationName !== 'IntrospectionQuery') {
     const { body } = req
-    winstonLog.info(`${process.env.NODE_ENV} requests`, { req, res, body })
+    winstonLog().info(`${process.env.NODE_ENV} requests`, { req, res, body })
   }
   next()
 }
