0185b51577e2a54a1c5159e5c0e5de9cba144bc8
Ashaka Egerega <ashakaegerega@gmail.com>
2023-03-22 17:13:24 +0100

    Fix missing / for regex (#333)

--- src/App/utils/customValidator.ts
+++ src/App/utils/customValidator.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   ValidatorConstraint,
@@ -5,10 +6,10 @@ import {
   ValidationArguments,
 } from 'class-validator'
 
-@ValidatorConstraint({ name: 'customText', async: false })
+@ValidatorConstraint({ name: 'valid String', async: true })
 export default class ValidStringParams implements ValidatorConstraintInterface {
-  validate(text: string, args: ValidationArguments) {
-    return /^[a-zA-Z0-9-]*$/.test(text)
+  async validate(text: string, args: ValidationArguments) {
+    return /^[a-zA-Z0-9\/-]*$/.test(text)
   }
 
   defaultMessage(args: ValidationArguments) {
