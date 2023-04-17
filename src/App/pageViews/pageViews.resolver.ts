16612548337ec60e1ee01a41d13d35a1535127ad
AshakaE <ashakaegerega@gmail.com>
2023-04-17 18:24:17 +0100

    Change return type

--- src/App/pageViews/pageViews.resolver.ts
+++ src/App/pageViews/pageViews.resolver.ts
@@ -2,10 +2,10 @@ import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { UseInterceptors, UseGuards } from '@nestjs/common'
 import PageViewsService from './pageViews.service'
 import { ArgsById } from '../utils/queryHelpers'
-import PageviewsPerDay from '../../Database/Entities/pageviewsPerPage.entity'
 import AuthGuard from '../utils/admin.guard'
 import AdminLogsInterceptor from '../utils/adminLogs.interceptor'
 import PaginationArgs from '../pagination.args'
+import { WikiViews } from './pageviews.dto'
 
 @UseInterceptors(AdminLogsInterceptor)
 @Resolver(() => Number)
@@ -17,7 +17,7 @@ class PageViewsResolver {
     return this.pageViewsService.updateCount(args.id, ctx.req.ip)
   }
 
-  @Query(() => [PageviewsPerDay])
+  @Query(() => [WikiViews])
   @UseGuards(AuthGuard)
   async wikiViews(@Args() args: PaginationArgs) {
     return this.pageViewsService.getWikiViews(args)
