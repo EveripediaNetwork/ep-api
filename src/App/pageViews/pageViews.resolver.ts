2a6c311d81f70f96d329637b95595cbcf8941917
Ashaka Egerega <ashakaegerega@gmail.com>
2023-04-18 11:36:52 +0100

    Token pages (#358)
    * Add wiki views query endpoint
    
    * Resolve tests
    
    * Add wiki views query endpoint
    
    * Change return type
    
    * Switch search order
    
    * Lint

--- src/App/pageViews/pageViews.resolver.ts
+++ src/App/pageViews/pageViews.resolver.ts
@@ -1,17 +1,26 @@
-import { Args, Context, Mutation, Resolver } from '@nestjs/graphql'
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { UseInterceptors, UseGuards } from '@nestjs/common'
 import PageViewsService from './pageViews.service'
 import { ArgsById } from '../utils/queryHelpers'
+import AuthGuard from '../utils/admin.guard'
+import AdminLogsInterceptor from '../utils/adminLogs.interceptor'
+import PaginationArgs from '../pagination.args'
+import { WikiViews } from './pageviews.dto'
 
+@UseInterceptors(AdminLogsInterceptor)
 @Resolver(() => Number)
 class PageViewsResolver {
   constructor(private pageViewsService: PageViewsService) {}
 
   @Mutation(() => Number)
-  async wikiViewCount(
-    @Args() args: ArgsById,
-    @Context() ctx: any,
-  ) {
+  async wikiViewCount(@Args() args: ArgsById, @Context() ctx: any) {
     return this.pageViewsService.updateCount(args.id, ctx.req.ip)
+  }
+
+  @Query(() => [WikiViews])
+  @UseGuards(AuthGuard)
+  async wikiViews(@Args() args: PaginationArgs) {
+    return this.pageViewsService.getWikiViews(args)
   }
 }
 
