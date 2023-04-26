fa693bb126ef314ed39eb64de6ac55868f977776
Ashaka Egerega <ashakaegerega@gmail.com>
2023-04-26 18:52:47 +0100

    Userservice fix (#367)
    * Add wiki views query endpoint
    
    * Resolve tests
    
    * Add wiki views query endpoint
    
    * Change return type
    
    * Select columns only
    
    * Resolve changes
    
    * Update getColumns
    
    * Lint
    
    * Remove logs

--- src/App/User/user.service.ts
+++ src/App/User/user.service.ts
@@ -157,7 +157,12 @@ class UserService {
 
     const columnNames = userTable.columns.map((column) => column.propertyName)
     const columns = columnNames.filter((e) => fields.includes(e))
-    const fieldsWithPrefix = columns.map((field) => `${tableName}.${field}`)
+    const fieldsWithPrefix = columns.map((field) => {
+      if (!columns.includes('id')) {
+        return `${tableName}.id`
+      }
+      return `${tableName}.${field}`
+    })
 
     return fieldsWithPrefix
   }
