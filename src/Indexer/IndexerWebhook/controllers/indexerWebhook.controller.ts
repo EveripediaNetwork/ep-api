ca1296f1593dd212671d6bf1b70c7a95c3f71ec9
AshakaE <ashakaegerega@gmail.com>
2023-05-25 16:23:13 +0100

    Test HMAC signing for webhook security

--- src/Indexer/IndexerWebhook/controllers/indexerWebhook.controller.ts
+++ src/Indexer/IndexerWebhook/controllers/indexerWebhook.controller.ts
@@ -1,27 +1,46 @@
-import { Body, Controller, Post, Req } from '@nestjs/common'
+import { Controller, HttpStatus, Post, Req, Body, Res } from '@nestjs/common'
+import { Response } from 'express'
 import { ConfigService } from '@nestjs/config'
+import * as crypto from 'crypto'
 
 @Controller('indexer')
 class IndexerWebhookController {
   constructor(private configService: ConfigService) {}
 
-  private keys() {
-    const key = this.configService.get<string>('INDEXER_API_KEY')
-    const secret = this.configService.get<string>('ALCHEMY_WEBHOOK_KEY')
-    return { key, secret }
+  private signingKey() {
+    return this.configService.get<string>('WEBHOOK_SIGNNING_KEY')
   }
 
-  @Post('/webhook')
-  async initiateStore(@Req() request: any, @Body() value: string) {
-    const apiKey: string = request.headers['i-api-key']
-    const alchemyKey: string = request.headers['i-alchemy-key']
+  @Post()
+  async initiateStore(
+    @Req() request: any,
+    @Res() res: Response,
+    @Body() value: string,
+  ) {
+    const signature = request.headers['x-alchemy-signature']
+    const key = this.signingKey()
+    const validateRequest = await this.isValidSignatureForStringBody(
+      JSON.stringify(value),
+      signature,
+      key
+    )
 
-    if (apiKey === this.keys().key && alchemyKey === this.keys().secret) {
-      console.log(apiKey)
-      console.log(alchemyKey)
-    } else {
-      console.log(false)
+    if (validateRequest) {
+      return res.status(HttpStatus.OK).json({ valid: true })
     }
+    return res.status(HttpStatus.BAD_REQUEST).json({ valid: false })
+  }
+
+  async isValidSignatureForStringBody(
+    body: string,
+    signature: string,
+    signingKey = 'whsec_test',
+  ): Promise<boolean> {
+    const hmac = crypto.createHmac('sha256', signingKey)
+    hmac.update(body, 'utf8')
+    const digest = hmac.digest('hex')
+
+    return signature === digest
   }
 }
 export default IndexerWebhookController
