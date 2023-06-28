9f69a6366026ca8785136a56a9deb2f36dad94cd
AshakaE <ashakaegerega@gmail.com>
2023-06-28 10:13:35 +0100

    Setup basic treasury store

--- src/App/Treasury/treasury.repository.ts
+++ src/App/Treasury/treasury.repository.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common'
-import { Repository } from 'typeorm'
+import { DataSource, Repository } from 'typeorm'
 import Treasury from '../../Database/Entities/treasury.entity'
 
 @Injectable()
-class TreasuryRepository extends Repository<Treasury> {}
+class TreasuryRepository extends Repository<Treasury> {
+  constructor(private dataSource: DataSource) {
+    super(Treasury, dataSource.createEntityManager())
+  }
+
+  async saveData(tokenValue: string): Promise<Treasury> {
+    const newTreasuryValue = this.create({ totalValue: tokenValue })
+    return this.save(newTreasuryValue)
+  }
+}
 
 export default TreasuryRepository
--- src/App/Treasury/treasury.service.ts
+++ src/App/Treasury/treasury.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common'
-import { Cron } from '@nestjs/schedule'
+import { Cron, CronExpression } from '@nestjs/schedule'
+import TreasuryRepository from './treasury.repository'
 
 @Injectable()
 class TreasuryService {
-  @Cron('*/10 * * * * *') // Cron schedule expression (runs every 10 seconds)
-  handleCron() {
-    console.log('Done')
+    constructor(private repo: TreasuryRepository) {
+
+    }
+//   private counter = 0
+
+//   @Cron(CronExpression.EVERY_10_SECONDS)
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  async storeToalValue() {
+    // this.counter += 1
+    // console.log(`Done ${this.counter}`)
+    await this.repo.saveData('10000000000') 
+    // console.log('done')
   }
 }
 export default TreasuryService
