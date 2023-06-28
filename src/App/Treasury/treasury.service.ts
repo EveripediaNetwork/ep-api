3552c46182fc633fbdf166f7db636743f0c8af2e
AshakaE <ashakaegerega@gmail.com>
2023-06-28 10:15:42 +0100

    Lint

--- src/App/Treasury/treasury.service.ts
+++ src/App/Treasury/treasury.service.ts
@@ -4,17 +4,15 @@ import TreasuryRepository from './treasury.repository'
 
 @Injectable()
 class TreasuryService {
-    constructor(private repo: TreasuryRepository) {
+  constructor(private repo: TreasuryRepository) {}
+  //   private counter = 0
 
-    }
-//   private counter = 0
-
-//   @Cron(CronExpression.EVERY_10_SECONDS)
+  //   @Cron(CronExpression.EVERY_10_SECONDS)
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async storeToalValue() {
     // this.counter += 1
     // console.log(`Done ${this.counter}`)
-    await this.repo.saveData('10000000000') 
+    await this.repo.saveData('10000000000')
     // console.log('done')
   }
 }
