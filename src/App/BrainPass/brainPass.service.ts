f224f023b9a9c5d39fd1b76558f488728d96991c
AshakaE <ashakaegerega@gmail.com>
2023-08-02 19:24:26 +0100

    Update brainpass

--- src/App/BrainPass/brainPass.service.ts
+++ src/App/BrainPass/brainPass.service.ts
@@ -49,11 +49,12 @@ class BrainPassService {
 
   async storeMintData(eventLog: any): Promise<void> {
     console.log('Signature verified 🎟️ for NFT')
+    
     const { image, description } = await this.getDefaultData()
-    if(!eventLog || !eventLog.transaction) return
-    console.log(eventLog)
+    if (!eventLog || !eventLog.transaction) return
+
     const { logs, hash } = eventLog.transaction
-    console.log(logs)
+
     const data = await this.decodeNFTEvent(logs[logs.length - 2])
 
     let ipfshash
