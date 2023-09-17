05afc390cb124c52b8614a484d44d8afabb4081d
AshakaE <ashakaegerega@gmail.com>
2023-09-17 08:58:49 +0100

    Enable service

--- src/App/IQHolders/IQHolder.service.ts
+++ src/App/IQHolders/IQHolder.service.ts
@@ -52,31 +52,30 @@ class IQHolderService {
     })
   }
 
-  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, {disabled: true})
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async checkForNewHolders() {
-    // const job = this.schedulerRegistry.getCronJob('storeIQHolderCount')
-    // if (firstLevelNodeProcess() && !job) {
-    //   await this.indexIQHolders()
-    // }
+    const job = this.schedulerRegistry.getCronJob('storeIQHolderCount')
+    if (firstLevelNodeProcess() && !job) {
+      await this.indexIQHolders()
+    }
   }
 
   @Cron(CronExpression.EVERY_MINUTE, {
-    name: cronIndexerId,
-    disabled: true
+    name: cronIndexerId
   })
   async storeIQHolderCount() {
-    // const tempStop: boolean | undefined = await this.cacheManager.get(
-    //   cronIndexerId,
-    // )
-    // const today = new Date()
-    // const oneDayBack = new Date(today)
-    // oneDayBack.setDate(oneDayBack.getDate() - 1)
-    // const job = this.schedulerRegistry.getCronJob('storeIQHolderCount')
-    // const jobRun = await stopJob(this.repo, job, oneDayBack)
-    // if (tempStop) return
-    // if (firstLevelNodeProcess() && !jobRun) {
-    //   await this.indexIQHolders()
-    // }
+    const tempStop: boolean | undefined = await this.cacheManager.get(
+      cronIndexerId,
+    )
+    const today = new Date()
+    const oneDayBack = new Date(today)
+    oneDayBack.setDate(oneDayBack.getDate() - 1)
+    const job = this.schedulerRegistry.getCronJob('storeIQHolderCount')
+    const jobRun = await stopJob(this.repo, job, oneDayBack)
+    if (tempStop) return
+    if (firstLevelNodeProcess() && !jobRun) {
+      await this.indexIQHolders()
+    }
   }
 
   async indexIQHolders() {
