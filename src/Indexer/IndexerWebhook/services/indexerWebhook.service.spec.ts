df0ed4a99798aec0de0fa8dab81e94f7d6d7c456
AshakaE <ashakaegerega@gmail.com>
2023-10-13 12:57:19 +0100

    Lint

--- src/Indexer/IndexerWebhook/services/indexerWebhook.service.spec.ts
+++ src/Indexer/IndexerWebhook/services/indexerWebhook.service.spec.ts
@@ -93,7 +93,7 @@ describe('IndexerWebhookService', () => {
           ProviderEnum.metadataChangesService,
           ProviderEnum.indexerWebhookService,
           ProviderEnum.revalidatePageService,
-          ProviderEnum.eventEmitter2
+          ProviderEnum.eventEmitter2,
         ]),
         {
           provide: ConfigService,
