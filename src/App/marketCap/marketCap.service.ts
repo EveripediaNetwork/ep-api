f8074ba80519600c4565b02b3dc79bbd66fc1d0a
Cold Dev <teniopemipo@gmail.com>
2024-01-15 14:40:37 +0100

    Added Nft

--- src/App/marketCap/marketCap.service.ts
+++ src/App/marketCap/marketCap.service.ts
@@ -267,13 +267,24 @@ class MarketCapService {
     id: string,
     category: 'cryptocurrencies' | 'nft',
   ): Promise<any> {
-    const wikiRepository = this.dataSource.getRepository(Wiki)
-
+    const wikiRepository = this.dataSource.getRepository(Wiki);
+  
+    let coingeckoProfileUrl: string;
+  
+    if (category === 'cryptocurrencies') {
+      coingeckoProfileUrl = `https://www.coingecko.com/en/coins/${id}`;
+    } else if (category === 'nft') {
+      coingeckoProfileUrl = `https://www.coingecko.com/en/nft/${id}`;
+    } else {
+      
+      return null;
+    }
+  
     const wiki = await wikiRepository
       .createQueryBuilder('wiki')
       .where('wiki.id = :id AND wiki.hidden = false', { id })
       .andWhere(`wiki.coingecko_profile->>'value' = :url`, {
-        url: `https://www.coingecko.com/en/coins/${id}`,
+        url: coingeckoProfileUrl,
       })
       .innerJoinAndSelect(
         'wiki.categories',
@@ -281,10 +292,11 @@ class MarketCapService {
         'category.id = :categoryId',
         { categoryId: category },
       )
-      .getOne()
-
-    return wiki
+      .getOne();
+  
+    return wiki;
   }
+  
 }
 
 export default MarketCapService
