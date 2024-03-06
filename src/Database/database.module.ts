624a6e86625ee874ecd18f11f60840388ac236e7
AshakaE <ashakaegerega@gmail.com>
2024-03-06 12:45:41 +0100

    View db query on error

--- src/Database/database.module.ts
+++ src/Database/database.module.ts
@@ -55,7 +55,7 @@ import MarketCapIds from './Entities/marketCapIds.entity'
         ],
         synchronize: true,
         keepConnectionAlive: true,
-        // logging: true,
+        logging: ["error"]
       }),
       inject: [ConfigService],
     }),
