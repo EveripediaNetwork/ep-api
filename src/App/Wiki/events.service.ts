ad1df5ec1e9d2932aa5c199d66afca37bd98c2ca
AshakaE <ashakaegerega@gmail.com>
2024-03-13 16:59:03 +0100

    Lint

--- src/App/Wiki/events.service.ts
+++ src/App/Wiki/events.service.ts
@@ -32,7 +32,7 @@ class EventsService {
       .createQueryBuilder('wiki')
       .leftJoinAndSelect('wiki.tags', 'tag')
       .where('LOWER(tag.id) IN (:...tags)', {
-        tags: ids.map(tag => tag.toLowerCase()),
+        tags: ids.map((tag) => tag.toLowerCase()),
       })
       .andWhere('wiki.hidden = false')
       .andWhere('LOWER(tag.id) = LOWER(:ev)', { ev: eventTag })
@@ -59,7 +59,7 @@ class EventsService {
     ids: string[],
     args: EventArgs,
   ) {
-    const lowerCaseIds = ids.map(tag => tag.toLowerCase())
+    const lowerCaseIds = ids.map((tag) => tag.toLowerCase())
     const order =
       args.order === 'date'
         ? `"subquery"."events"->0->>'date'`
