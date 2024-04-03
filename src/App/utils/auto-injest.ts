3506cc64f2bce189f85f29a03c1891dfe0dbaf66
AshakaE <ashakaegerega@gmail.com>
2024-04-03 12:00:01 +0100

    Lint

--- src/App/utils/auto-injest.ts
+++ src/App/utils/auto-injest.ts
@@ -1,7 +1,6 @@
 import { HttpService } from '@nestjs/axios'
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { LockingService } from '../IQHolders/IQHolders.dto'
 
 export const injestId = 'injest.action'
 @Injectable()
@@ -9,77 +8,30 @@ class AutoInjestService {
   constructor(
     private configService: ConfigService,
     private httpService: HttpService,
-    private lockingService: LockingService,
   ) {}
 
-  private INJEST_KEYS(): string {
-    return this.configService.get<string>('INDEXER_GITHUB_TOKEN') as string
-  }
-
   async initiateInjest() {
-    const status = await this.checkWorkflowStatus()
-
-    const environment = this.configService.get<string>('API_LEVEL') as string
-
-    const locked = await this.lockingService.acquireLock(injestId)
-    console.debug({
-      injest_wait: locked,
-      workflow_status: status,
-    })
-    if (environment === 'dev' || !locked || !status) return
-
+    const scraperUrl = this.configService.get<string>(
+      'TRIGGER_SCRAPPER_URL',
+    ) as string
+    const statusUrl = this.configService.get<string>(
+      'TRIGGER_SCRAPPER_STATUS',
+    ) as string
     try {
-      const payload = {
-        event_type: 'trigger-scraper',
-        client_payload: {
-          scraperName: 'IQ Wiki',
-        },
+      const statusResponse = await this.httpService.get(statusUrl).toPromise()
+
+      if (!statusResponse?.data.state) {
+        setTimeout(async () => {
+          await this.initiateInjest()
+        }, 30000)
+        return
       }
 
-      await this.httpService
-        .post(
-          'https://api.github.com/repos/EveripediaNetwork/iq-gpt-ingester-js/dispatches',
-          payload,
-          {
-            headers: {
-              Accept: 'application/vnd.github.v3+json',
-              Authorization: `token ${this.INJEST_KEYS()}`,
-            },
-          },
-        )
-        .toPromise()
+      await this.httpService.post(scraperUrl).toPromise()
       console.log('Injest action emitted ! ! ⚙️')
-    } catch (e) {
+    } catch (e: any) {
       console.error(e)
     }
-
-    setTimeout(() => {
-      this.lockingService.releaseLock(injestId)
-    }, 30000)
-  }
-
-  async checkWorkflowStatus(): Promise<boolean> {
-    let status = false
-    try {
-      const response = await this.httpService
-        .get(
-          'https://api.github.com/repos/EveripediaNetwork/iq-gpt-ingester-js/actions/workflows/trigger.yml/runs',
-
-          {
-            headers: {
-              Accept: 'application/vnd.github.v3+json',
-              Authorization: `token ${this.INJEST_KEYS()}`,
-            },
-          },
-        )
-        .toPromise()
-
-      status = response?.data.workflow_runs[0].status === 'completed'
-    } catch (e) {
-      console.error(e)
-    }
-
-    return status
   }
 }
 
