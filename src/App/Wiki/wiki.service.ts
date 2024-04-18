2f8cdbed39c8d01032dd41ce0d3bb4ac0dc7b3db
AshakaE <ashakaegerega@gmail.com>
2024-04-18 19:48:36 +0100

    Lint

--- src/App/Wiki/wiki.service.ts
+++ src/App/Wiki/wiki.service.ts
@@ -202,7 +202,7 @@ class WikiService {
 
     if (typeof queryBuilder !== 'string') {
       return queryBuilder.andWhere(
-        new Brackets(qb => {
+        new Brackets((qb) => {
           qb.orWhere("wiki.events->0->>'type' IS NULL").orWhere(
             "wiki.events->0->>'type' = 'DEFAULT'",
           )
@@ -211,7 +211,7 @@ class WikiService {
               start: startDate,
               end: endDate,
             }).orWhere(
-              new Brackets(qb2 => {
+              new Brackets((qb2) => {
                 qb2
                   .where("wiki.events->0->>'type' = 'MULTIDATE'")
                   .andWhere(
@@ -393,7 +393,9 @@ class WikiService {
           : []
 
       for (const promotedWiki of promotedWikis) {
-        await (await this.repository())
+        await (
+          await this.repository()
+        )
           .createQueryBuilder()
           .update(Wiki)
           .set({ promoted: 0 })
@@ -401,7 +403,9 @@ class WikiService {
           .execute()
       }
 
-      await (await this.repository())
+      await (
+        await this.repository()
+      )
         .createQueryBuilder()
         .update(Wiki)
         .set({ promoted: args.level })
@@ -414,7 +418,9 @@ class WikiService {
 
   async hideWiki(args: ByIdArgs): Promise<Wiki | null> {
     const wiki = (await this.repository()).findOneBy({ id: args.id })
-    await (await this.repository())
+    await (
+      await this.repository()
+    )
       .createQueryBuilder()
       .update(Wiki)
       .set({ hidden: true, promoted: 0 })
@@ -444,7 +450,9 @@ class WikiService {
 
   async unhideWiki(args: ByIdArgs): Promise<Wiki | null> {
     const wiki = (await this.repository()).findOneBy({ id: args.id })
-    await (await this.repository())
+    await (
+      await this.repository()
+    )
       .createQueryBuilder()
       .update(Wiki)
       .set({ hidden: false })
@@ -470,7 +478,9 @@ class WikiService {
   async getCategoryTotal(args: CategoryArgs): Promise<Count | undefined> {
     const count: any | undefined = await this.cacheManager.get(args.category)
     if (count) return count
-    const response = await (await this.repository())
+    const response = await (
+      await this.repository()
+    )
       .createQueryBuilder('wiki')
       .select('Count(wiki.id)', 'amount')
       .innerJoin('wiki_categories_category', 'wc', 'wc."wikiId" = wiki.id')
@@ -495,7 +505,7 @@ class WikiService {
         fullLinkedWikis.push(f)
       }
     }
-    return fullLinkedWikis.filter(item => item !== null)
+    return fullLinkedWikis.filter((item) => item !== null)
   }
 
   async getPopularEvents(args: EventDefaultArgs) {
