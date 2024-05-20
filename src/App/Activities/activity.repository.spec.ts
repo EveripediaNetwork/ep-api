0bc46c21a2be820388d74a7306b165e72f1d918b
Cold Dev <teniopemipo@gmail.com>
2024-05-20 03:28:34 +0100

    Fixed all failing tests

--- src/App/Activities/activity.repository.spec.ts
+++ src/App/Activities/activity.repository.spec.ts
@@ -5,34 +5,32 @@ import ActivityRepository from './activity.repository'
 import ActivityService from './activity.service'
 import Activity from '../../Database/Entities/activity.entity'
 // import { ActivityByCategoryArgs } from './dto/activity.dto'
-// import { expectedQuery, selections } from './activity.service.spec'
+import { expectedQuery, selections } from './activity.service.spec'
 import { Count, UserArgs, WikiStats } from '../Wiki/wikiStats.dto'
-import { ActivityByCategoryArgs } from './dto/activity.dto'
+import { ActivityArgsByUser, ActivityByCategoryArgs } from './dto/activity.dto'
 import { ActivityType } from '../general.args'
 
 describe('CategoryService', () => {
   let repository: ActivityRepository
-  //   let service: ActivityService
+  let service: ActivityService
   let moduleRef: TestingModule
 
-  //   const createQueryBuilder = jest.fn().mockReturnThis()
   const select = jest.fn().mockReturnThis()
   const addSelect = jest.fn().mockReturnThis()
   const leftJoin = jest.fn().mockReturnThis()
-  //   const leftJoinAndSelect = jest.fn().mockReturnThis()
+  const leftJoinAndSelect = jest.fn().mockReturnThis()
   const where = jest.fn().mockReturnThis()
   const andWhere = jest.fn().mockReturnThis()
-  //   const cache = jest.fn().mockReturnThis()
-  //   const limit = jest.fn().mockReturnThis()
-  //   const offset = jest.fn().mockReturnThis()
+  const limit = jest.fn().mockReturnThis()
+  const offset = jest.fn().mockReturnThis()
   const orderBy = jest.fn().mockReturnThis()
   const groupBy = jest.fn().mockReturnThis()
   const setParameters = jest.fn().mockReturnThis()
-  //   const getOne = jest.fn().mockResolvedValue({})
-  //   const query = jest.fn().mockResolvedValue({})
-  //   const getRawOne = jest.fn().mockResolvedValue({})
-  //   const getRawMany = jest.fn().mockResolvedValue([])
-  //   let getMany = jest.fn().mockResolvedValue([])
+  const cache = jest.fn().mockReturnThis()
+  const getOne = jest.fn().mockResolvedValue(new Activity())
+  const getRawOne = jest.fn().mockResolvedValue({})
+  const getRawMany = jest.fn().mockResolvedValue([])
+  let getMany = jest.fn().mockResolvedValue([])
 
   const args = {
     wikiId: 'example-wiki-id',
@@ -62,7 +60,6 @@ describe('CategoryService', () => {
     }).compile()
 
     repository = moduleRef.get<ActivityRepository>(ActivityRepository)
-    // service = moduleRef.get<ActivityService>(ActivityService)
   })
 
   describe('countUserActivity', () => {
@@ -169,11 +166,134 @@ describe('CategoryService', () => {
       )
     })
   })
+  describe('getActivitiesByUser', () => {
+    it('should return activities by user', async () => {
+      const userArgs = {
+        userId: '0x5456afEA3aa035088Fe1F9Aa36509B320360a89e',
+        offset: 0,
+        limit: 10,
+        lang: 'en',
+      }
+      const query = `
+      query {
+        activitiesByUser {
+          id
+          user
+          content {
+            id
+            title
+          }
+        }
+      }
+    `
+      const fields = ['field1', 'field2']
+      const expectedActivities = [
+        {
+          id: undefined,
+          user: undefined,
+          content: [
+            {
+              id: undefined,
+              title: undefined,
+              author: { id: null },
+              user: { id: undefined },
+              block: undefined,
+              categories: undefined,
+              created: undefined,
+              images: undefined,
+              ipfs: undefined,
+              media: undefined,
+              metadata: undefined,
+              summary: undefined,
+              tags: undefined,
+              transactionHash: undefined,
+              updated: undefined,
+              version: undefined,
+              content: undefined,
+            },
+          ],
+          a_author: undefined,
+          a_block: undefined,
+          a_categories: undefined,
+          a_content: undefined,
+          a_created: undefined,
+          a_title: undefined,
+          a_transactionHash: undefined,
+          a_updated: undefined,
+          a_version: undefined,
+          block: undefined,
+          created_timestamp: undefined,
+          datetime: undefined,
+          forId: undefined,
+          ipfs: undefined,
+        },
+      ]
+
+      repository.createQueryBuilder = jest.fn().mockReturnValue({
+        select,
+        leftJoin,
+        leftJoinAndSelect: jest.fn().mockReturnThis(),
+        where,
+        limit: jest.fn().mockReturnThis(),
+        offset: jest.fn().mockReturnThis(),
+        orderBy: jest.fn().mockReturnThis(),
+        getMany: jest.fn().mockResolvedValue(expectedActivities),
+      })
+
+      const result = await repository.getActivitiesByUser(
+        userArgs,
+        query,
+        fields,
+      )
+
+      expect(result).toEqual(expectedActivities)
+      expect(repository.createQueryBuilder).toHaveBeenCalledWith('activity')
+      expect(leftJoin).toHaveBeenCalledWith(
+        'wiki',
+        'w',
+        'w."id" = activity.wikiId',
+      )
+      expect(where).toHaveBeenCalledWith(
+        'w."hidden" = false AND activity.user = :id',
+        {
+          lang: userArgs.lang,
+          id: userArgs.userId,
+        },
+      )
+    })
+  })
+
+  describe('getActivitiesById', () => {
+    it('should return activity by ID', async () => {
+      const id = 'binance-coin'
+      const expectedActivity = new Activity()
+
+      repository.createQueryBuilder = jest.fn().mockReturnValue({
+        leftJoin: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        getOne: jest.fn().mockResolvedValue(expectedActivity),
+      })
+
+      const result = await repository.getActivitiesById(id)
+
+      expect(result).toEqual(expectedActivity)
+      expect(repository.createQueryBuilder).toHaveBeenCalledWith('activity')
+      expect(repository.createQueryBuilder().leftJoin).toHaveBeenCalledWith(
+        'wiki',
+        'w',
+        'w."id" = activity.wikiId',
+      )
+      expect(repository.createQueryBuilder().where).toHaveBeenCalledWith(
+        'activity.id = :id AND w."hidden" = false',
+        { id },
+      )
+    })
+  })
 
   describe('getActivitiesByWikiIdAndBlock', () => {
     it('should return activity by Wiki ID and block', async () => {
       const blockArgs = {
-        wikiId: 'example-wiki-id',
+        wikiId: 'binance-coin',
         lang: 'en',
         block: 102102,
         offset: 0,
