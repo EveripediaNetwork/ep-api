0ddda21c8d269783d911b4e6998db528a6712950
AshakaE <ashakaegerega@gmail.com>
2024-07-25 10:38:47 +0100

    Remove optional ip for flagWiki

--- src/App/flaggingSystem/flagWiki.resolver.ts
+++ src/App/flaggingSystem/flagWiki.resolver.ts
@@ -1,11 +1,4 @@
-import {
-  Args,
-  ArgsType,
-  Context,
-  Field,
-  Mutation,
-  Resolver,
-} from '@nestjs/graphql'
+import { Args, ArgsType, Field, Mutation, Resolver } from '@nestjs/graphql'
 import { Validate } from 'class-validator'
 import ValidStringParams from '../utils/customValidator'
 import FlagWikiService from './flagWiki.service'
@@ -30,11 +23,8 @@ class FlagWikiResolver {
   constructor(private flagWikiService: FlagWikiService) {}
 
   @Mutation(() => Boolean)
-  async flagWiki(@Context() ctx: any, @Args() args: FlagWikiArgs) {
-    await this.flagWikiService.flagWiki({
-      ...args,
-      userId: args.userId || ctx.req.ip,
-    })
+  async flagWiki(@Args() args: FlagWikiArgs) {
+    await this.flagWikiService.flagWiki(args)
     return true
   }
 }
