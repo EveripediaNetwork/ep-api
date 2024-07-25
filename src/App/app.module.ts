146bd49ca8d0b18d19e30224c5c24b96ed1dc552
icemedia001 <teniopemipo@gmail.com>
2024-07-24 14:54:16 -1200

    Created and upgraded new branch for posthog integration

--- src/App/app.module.ts
+++ src/App/app.module.ts
@@ -65,6 +65,7 @@ import EventsResolver from './Wiki/events.resolver'
 import TagRepository from './Tag/tag.repository'
 import EventsService from './Wiki/events.service'
 import AppService from './app.service'
+import { PosthogModule } from 'nestjs-posthog'
 
 // istanbul ignore next
 @Module({
@@ -91,6 +92,24 @@ import AppService from './app.service'
           }),
         ],
       },
+    }),
+    PosthogModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const apiKey = configService.get<string>('POSTHOG_API_KEY')
+        const host = configService.get<string>('POSTHOG_API_URL')
+        if (!apiKey ||!host) {
+          console.error('Posthog configuration is missing apiKey or host')
+        }
+        return {
+          apiKey: apiKey || '',
+          options: {
+            host,
+          },
+          mock: true,
+        }
+        },
     }),
     SitemapModule,
     MailerModule,
