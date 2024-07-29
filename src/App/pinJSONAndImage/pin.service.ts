513b29656de214bb7433e44f58a98590dcc76dd4
Ashaka Egerega <ashakaegerega@gmail.com>
2024-07-29 12:03:00 +0100

    Rankpage (#569)
    * Use fake agents/headers
    
    * Remove dummy file

--- src/App/pinJSONAndImage/pin.service.ts
+++ src/App/pinJSONAndImage/pin.service.ts
@@ -187,6 +187,21 @@ class PinService {
       const response = await this.httpService
         .get(url, {
           responseType: 'text',
+          headers: {
+            'User-Agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0',
+            Accept:
+              'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate',
+            Connection: 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+            'Cache-Control': 'max-age=0',
+          },
         })
         .toPromise()
 
