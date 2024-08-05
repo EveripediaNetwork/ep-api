067e77e032258536059c0c015a52f99d25ae5a7f
AshakaE <ashakaegerega@gmail.com>
2024-08-05 13:51:51 +0100

    Update api id check

--- src/App/pinJSONAndImage/pin.service.ts
+++ src/App/pinJSONAndImage/pin.service.ts
@@ -145,7 +145,7 @@ class PinService {
     saveMatchedIdcallback: () => Promise<void | MarketCapIds>
   }> {
     const coingeckoProfileMetadata = wiki.metadata.find(
-      (e) => e.id === 'coingecko_profile',
+      e => e.id === 'coingecko_profile',
     )
 
     if (!coingeckoProfileMetadata) {
@@ -165,7 +165,7 @@ class PinService {
       }
 
       const index = wiki.metadata.findIndex(
-        (item) => item.id === 'coingecko_profile',
+        item => item.id === 'coingecko_profile',
       )
 
       if (index !== -1) {
@@ -213,6 +213,8 @@ class PinService {
         if (
           (!slugId.includes('-') &&
             coin.name.toLowerCase() === slugId.toLowerCase()) ||
+          coin.name.split(' ').join('-').toLowerCase() ===
+            slugId.toLowerCase() ||
           slugId === coin.id
         ) {
           apiId = coin.id
