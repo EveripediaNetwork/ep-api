3ce6a8569bb5ceb9c07403f3c57fa3ae5568296c
AshakaE <ashakaegerega@gmail.com>
2024-09-03 16:22:45 +0100

    Remove cache on promoted wikis, and  add filter for promoted events

--- src/App/Wiki/events.service.ts
+++ src/App/Wiki/events.service.ts
@@ -22,29 +22,17 @@ class EventsService {
       const queryBuilder = repository
         .createQueryBuilder('wiki')
         .innerJoin('wiki.tags', 'tag')
-        .leftJoinAndSelect('wiki.wikiEvents', 'wikiEvents')
-        .where('subWiki.hidden = :hidden', { hidden: false })
-        .limit(args.limit)
-        .offset(args.offset)
+        .leftJoin('wiki.wikiEvents', 'wikiEvents')
+        .where('wiki.hidden = :hidden', { hidden: false })
 
       if (ids.length > 1) {
-        queryBuilder.where((qb) => {
-          const subQuery = qb
-            .subQuery()
-            .select('subWiki.id')
-            .from(Wiki, 'subWiki')
-            .innerJoin('subWiki.tags', 'subTag')
-            .where('LOWER(subTag.id) = ANY(:tagIds)', {
-              tagIds: ids.map((tag) => tag.toLowerCase()),
-            })
-            .andWhere('subWiki.hidden = :hidden', { hidden: false })
-            .groupBy('subWiki.id')
-            .having('COUNT(DISTINCT subTag.id) > 1')
-            .getQuery()
-          return `wiki.id IN ${subQuery}`
-        })
+        queryBuilder
+          .andWhere('LOWER(tag.id) IN (:...ids)', {
+            ids: ids.map((id) => id.toLowerCase()),
+          })
+          .having('COUNT(DISTINCT tag."id") > 1')
       } else {
-        queryBuilder.where('LOWER(tag.id) = LOWER(:ev)', { ev: eventTag })
+        queryBuilder.andWhere('LOWER(tag.id) != LOWER(:ev)', { ev: eventTag })
       }
 
       this.wikiService.applyDateFilter(
@@ -66,7 +54,11 @@ class EventsService {
           break
       }
 
-      return await queryBuilder.getMany()
+      return await queryBuilder
+        .groupBy('wiki.id')
+        .limit(args.limit)
+        .offset(args.offset)
+        .getMany()
     } catch (error) {
       console.error('Error fetching events:', error)
       throw error
