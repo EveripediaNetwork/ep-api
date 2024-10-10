c3f7434deee2930ea822ee0f8520124456067a42
Ashaka Egerega <ashakaegerega@gmail.com>
2024-10-10 10:48:06 +0100

    Update wikisCreated & wikisEdited logic (#590)
    * Update wikisCreated & wikisEdited logic
    
    * Remove console log

--- src/App/User/user.dto.ts
+++ src/App/User/user.dto.ts
@@ -1,7 +1,17 @@
-import { ArgsType, Field } from '@nestjs/graphql'
+/* eslint-disable import/no-cycle */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable max-classes-per-file */
+import {
+  ArgsType,
+  createUnionType,
+  Field,
+  Int,
+  ObjectType,
+} from '@nestjs/graphql'
 import { Validate, MinLength } from 'class-validator'
 import PaginationArgs from '../pagination.args'
 import ValidStringParams from '../utils/customValidator'
+import Activity from '../../Database/Entities/activity.entity'
 
 @ArgsType()
 export class UserStateArgs {
@@ -30,10 +40,36 @@ export class UsersByEditArgs extends PaginationArgs {
 @ArgsType()
 export class GetProfileArgs {
   @Field({ nullable: true })
-    @Validate(ValidStringParams)
+  @Validate(ValidStringParams)
   id?: string
 
   @Field({ nullable: true })
-    @Validate(ValidStringParams)
+  @Validate(ValidStringParams)
   username?: string
 }
+
+@ObjectType()
+export class WikiCount {
+  @Field(() => Int, { defaultValue: 0 })
+  count!: number
+}
+
+@ObjectType()
+export class UserActivity {
+  @Field(() => [Activity], { nullable: true })
+  activity!: Activity[]
+}
+
+export const UserWikis = createUnionType({
+  name: 'UserWikis',
+  types: () => [UserActivity, WikiCount] as const,
+  resolveType(value) {
+    if (value.activity) {
+      return 'UserActivity'
+    }
+    if (value.count) {
+      return 'WikiCount'
+    }
+    return null
+  },
+})
