647dd1f5dfa274bdbfd524f235c152e22fde6618
Ashaka Egerega <ashakaegerega@gmail.com>
2024-11-08 09:47:31 +0100

    Adds resolver for hiiq stats by token rank, updates indexing logic to track tokens staked (#599)

--- src/App/HiIQHolders/hiIQHolder.resolver.ts
+++ src/App/HiIQHolders/hiIQHolder.resolver.ts
@@ -1,15 +1,22 @@
 import { Args, Query, Resolver } from '@nestjs/graphql'
 import HiIQHolder from '../../Database/Entities/hiIQHolder.entity'
-import HiIQHolderRepository from './hiIQHolder.repository'
 import HiIQHolderArgs from './hiIQHolders.dto'
+import HiIQHolderService from './hiIQHolder.service'
+import HiIQHolderAddress from '../../Database/Entities/hiIQHolderAddress.entity'
+import { OrderArgs } from '../pagination.args'
 
 @Resolver(() => HiIQHolder)
 class HiIQHoldersResolver {
-  constructor(private hiIQHoldersRepository: HiIQHolderRepository) {}
+  constructor(private hiIQHoldersService: HiIQHolderService) {}
 
   @Query(() => [HiIQHolder], { name: 'hiIQHolders' })
   async hiIQHolders(@Args() args: HiIQHolderArgs) {
-    return this.hiIQHoldersRepository.getHiIQHoldersCount(args)
+    return this.hiIQHoldersService.getHiIQHoldersCount(args)
+  }
+
+  @Query(() => [HiIQHolderAddress], { name: 'hiIQHoldersRank' })
+  async hiIQHoldersRank(@Args() args: OrderArgs) {
+    return this.hiIQHoldersService.hiIQHoldersRank(args)
   }
 }
 
