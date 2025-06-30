483235bb177abff4fdb616ad1e9242f157f43459
Adebesin Cell <adebesintolulope80@gmail.com>
2025-06-30 22:20:48 +0100

    fix: adds resolver to module export

--- src/App/Search/search.module.ts
+++ src/App/Search/search.module.ts
@@ -3,11 +3,12 @@ import { HttpModule } from '@nestjs/axios'
 import { ConfigModule } from '@nestjs/config'
 import SearchController from './search.controller'
 import SearchService from './search.service'
+import SearchResolver from './search.resolver'
 
 @Module({
   imports: [HttpModule, ConfigModule],
   controllers: [SearchController],
-  providers: [SearchService],
+  providers: [SearchService, SearchResolver],
   exports: [SearchService],
 })
 export default class SearchModule {}
