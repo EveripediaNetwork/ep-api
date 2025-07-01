549dfefae3e8cf3286c991e7cef9d58518692a45
AshakaE <ashakaegerega@gmail.com>
2025-07-01 13:53:19 +0100

    Update biome config

--- src/App/Search/search.module.ts
+++ src/App/Search/search.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common'
 import { HttpModule } from '@nestjs/axios'
 import { ConfigModule } from '@nestjs/config'
-import SearchController from './search.controller'
 import SearchService from './search.service'
 import SearchResolver from './search.resolver'
 
 @Module({
   imports: [HttpModule, ConfigModule],
-  controllers: [SearchController],
   providers: [SearchService, SearchResolver],
   exports: [SearchService],
 })
