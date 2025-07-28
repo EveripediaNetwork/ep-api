38db9e1d1acee915ab13f7b3022ea304f56c6daf
Aliu Salaudeen <ayokunumi99@gmail.com>
2025-07-28 14:01:46 +0100

    feat: Enhance SearchModule with additional providers and imports for improved functionality

--- src/App/Search/search.module.ts
+++ src/App/Search/search.module.ts
@@ -3,10 +3,13 @@ import { HttpModule } from '@nestjs/axios'
 import { ConfigModule } from '@nestjs/config'
 import SearchService from './search.service'
 import SearchResolver from './search.resolver'
+import WikiService from '../Wiki/wiki.service'
+import { ValidSlug } from '../utils/validSlug'
+import DiscordModule from '../utils/discord.module'
 
 @Module({
-  imports: [HttpModule, ConfigModule],
-  providers: [SearchService, SearchResolver],
+  imports: [HttpModule, ConfigModule, DiscordModule],
+  providers: [SearchService, SearchResolver, ValidSlug, WikiService],
   exports: [SearchService],
 })
 export default class SearchModule {}
